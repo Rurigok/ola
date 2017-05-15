e93728c2254482330eee62de087f15576659a3dd
sphassan <sphassan@sbcglobal.net>
2017-05-15 13:28:50 -0500

    Begin hashtable

--- hashtable.c
+++ hashtable.c
@@ -2,4 +2,28 @@
 // Created by andrew on 5/12/17.
 //
 
+#include <stdlib.h>
 #include "hashtable.h"
+
+hashtable * ht_create(int size)
+{
+    hashtable *ht = NULL;
+    int x;
+    
+    if (size < 1)
+        return NULL;
+    
+    /* memalloc table */
+    if ((ht -> table = malloc(sizeof(hashtable))) == NULL)
+        return NULL;
+    
+    /* memalloc entries */
+    if ((ht -> table = malloc(sizeof(entry *) * size)) == NULL)
+        return NULL;
+    for (x = 0; x < size; x++)
+        ht -> table[x] = NULL;
+    
+    ht -> size = size;
+    
+    return ht;
+}
--- hashtable.h
+++ hashtable.h
@@ -1,4 +1,17 @@
 #ifndef OLA_HASHTABLE_H
 #define OLA_HASHTABLE_H
 
+typedef struct entry_s {
+    void *key               // keys may be anything
+    void *value             // values may be anything
+    struct entry_s *next;
+} entry_t;
+
+typedef struct hashtable_s {
+    int size;
+    struct entry_t **table;
+} hashtable_t;
+
+hashtable * ht_create(int size);            // create a new hashtable
+
 #endif //OLA_HASHTABLE_H
