86a96df74f420147ea19320ecae1605f529da4e4
Andrew Sanetra <asanetras@gmail.com>
2017-05-15 15:52:21 -0500

    begin get/put for hashtable

--- hashtable.c
+++ hashtable.c
@@ -3,27 +3,108 @@
 //
 
 #include <stdlib.h>
+
+#include "error.h"
 #include "hashtable.h"
 
-hashtable * ht_create(int size)
-{
-    hashtable *ht = NULL;
-    int x;
-    
+hashtable_t * ht_create(int size) {
+
     if (size < 1)
         return NULL;
-    
+
+    hashtable_t *ht = NULL;
+
     /* memalloc table */
-    if ((ht -> table = malloc(sizeof(hashtable))) == NULL)
+    if ((ht = calloc(sizeof(hashtable_t), 1)) == NULL)
         return NULL;
     
     /* memalloc entries */
-    if ((ht -> table = malloc(sizeof(entry *) * size)) == NULL)
+    if ((ht -> table = calloc(sizeof(*entry_t), size)) == NULL)
         return NULL;
-    for (x = 0; x < size; x++)
-        ht -> table[x] = NULL;
     
     ht -> size = size;
     
     return ht;
+}
+
+void ht_put(hashtable_t *ht, void *key, unsigned int keySize, void *value) {
+
+    if (ht == NULL)
+        internalError("Attempt to put into uninitialized hash table");
+
+    if (key == NULL)
+        internalError("Attempt to put null key into hash table");
+
+    if (value == NULL)
+        internalError("Attempt to insert invalid value pointer into hash table");
+
+    // TODO: convert key into numeric key w/ nice hash algorithm
+    unsigned int hashKey = 0;
+
+    if (ht->table[hashKey] == NULL) {
+        // Slot is empty. Add a new entry here
+
+        entry_t *newEntry = malloc(sizeof(entry_t));
+
+        if (newEntry == NULL)
+            sysError("malloc failed to create ht entry");
+
+        newEntry->key = key;
+        newEntry->keySize = keySize;
+        newEntry->value = value;
+        newEntry->next = NULL;
+
+    } else {
+        // Collision! Add new entry at the end of the chain
+
+        entry_t *cur = ht->table[hashKey];
+
+        while (cur->next != NULL) {
+            cur = cur->next;
+        }
+        // cur is now the last entry_t in the chain
+
+        entry_t *newEntry = malloc(sizeof(entry_t));
+
+        if (newEntry == NULL)
+            sysError("malloc failed to create ht entry");
+
+        newEntry->key = key;
+        newEntry->keySize = keySize;
+        newEntry->value = value;
+        newEntry->next = NULL;
+
+        cur->next = newEntry;
+
+    }
+
+}
+
+void * ht_get(hashtable_t *ht, void *key, unsigned int keySize) {
+
+    if (ht == NULL)
+        internalError("Attempt to get from uninitialized hash table");
+
+    if (key == NULL)
+        internalError("Attempt to put null key into hash table");
+
+    // TODO: convert key into numeric key w/ nice hash algorithm
+    unsigned int hashKey = 0;
+
+    entry_t *found = ht->table[hashKey];
+
+    while (found != NULL) {
+
+        if (found->keySize == keySize && memcmp(found->key, key, keySize) == 0) {
+            return found->value;
+        }
+
+        found = found->next;
+
+    }
+
+    if (found == NULL)
+        // Did not find the given key
+        return NULL;
+
 }
--- hashtable.h
+++ hashtable.h
@@ -2,16 +2,21 @@
 #define OLA_HASHTABLE_H
 
 typedef struct entry_s {
-    void *key               // keys may be anything
-    void *value             // values may be anything
+    void *key;              // keys may be anything
+    unsigned int keySize;
+    void *value;            // values may be anything
     struct entry_s *next;
 } entry_t;
 
 typedef struct hashtable_s {
     int size;
-    struct entry_t **table;
+    struct entry_t *table[];
 } hashtable_t;
 
-hashtable * ht_create(int size);            // create a new hashtable
+hashtable_t * ht_create(int size);                      // create a new hashtable
+void ht_put(hashtable_t *ht, void *key, unsigned int keySize, void *value);   // put key/value pair into hash table
+void * ht_get(hashtable_t *ht, void *key, unsigned int keySize);              // get value by key
+void * ht_remove(hashtable_t *ht, void *key, unsigned int keySize);           // remove key/value pair by key
+void ht_clear(hashtable_t *ht);                         // delete all keys and values
 
 #endif //OLA_HASHTABLE_H
